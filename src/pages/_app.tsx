d0a50a086db8a6ead9c40681e0fd2dc1e341f573
ranselorm <ranselorm@outlook.com>
2026-06-26 22:37:31 +0100

    Add analytics to layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,9 @@ import Navbar from "@/components/Navbar";
 import { ModalProvider } from "@/context/ModalContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ContactModal from "@/components/ContactModal";
+import { Analytics } from "@vercel/analytics/next";
 
 const client = new QueryClient();
 
@@ -19,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <ModalProvider>
           <Navbar />
           <Component {...pageProps} />
+          <Analytics />
           <ContactModal />
           <Footer />
         </ModalProvider>
